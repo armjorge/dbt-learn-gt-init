f4a33cec0c7b2e2deb921fd0c15edeeeb50371ba
emma dalziell <emma.dalziell@dbtlabsworkshop.com>
2026-01-07 21:37:33 +0000

    create, staging files, get packages, rename and run

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,11 +1,11 @@
 with customers as (
 
     select
-        id as customer_id,
+        user_id as customer_id,
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
@@ -15,9 +15,9 @@ orders as (
         id as order_id,
         user_id as customer_id,
         order_date,
-        status
+        order_status
 
-    from raw.jaffle_shop.orders
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
